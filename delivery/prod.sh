b89a4c0c59f4ff2e28a4b08b6adc62a9bf5b2cf1
david <dumon.david@gmail.com>
2012-03-01 09:30:42 +0100

    delivery script

--- delivery/prod.sh
+++ delivery/prod.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+if [ $# -eq 0 ]
+then
+    echo "need the git tag token"
+    exit 1
+else
+    git tag $1
+    scp -P 22 ../index.html david@bytedojo.com:/var/lib/vz/private/109/var/www/
+    scp -P 22 ../contents/* david@bytedojo.com:/var/lib/vz/private/109/var/www/contents/
+    scp -P 22 ../css/* david@bytedojo.com:/var/lib/vz/private/109/var/www/css/
+    scp -P 22 ../js/* david@bytedojo.com:/var/lib/vz/private/109/var/www/js/
+    scp -P 22 ../images/* david@bytedojo.com:/var/lib/vz/private/109/var/www/images/
+fi
