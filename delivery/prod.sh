adc9c06bb9c222d73a887f0ade5a2cef82e66cbb
david <dumon.david@gmail.com>
2012-03-01 09:33:20 +0100

    good delivery script

--- delivery/prod.sh
+++ delivery/prod.sh
@@ -6,8 +6,8 @@ then
 else
     git tag $1
     scp -P 22 ../index.html david@bytedojo.com:/var/lib/vz/private/109/var/www/
-    scp -P 22 ../contents/* david@bytedojo.com:/var/lib/vz/private/109/var/www/contents/
-    scp -P 22 ../css/* david@bytedojo.com:/var/lib/vz/private/109/var/www/css/
-    scp -P 22 ../js/* david@bytedojo.com:/var/lib/vz/private/109/var/www/js/
-    scp -P 22 ../images/* david@bytedojo.com:/var/lib/vz/private/109/var/www/images/
+    scp -P 22 -r ../contents david@bytedojo.com:/var/lib/vz/private/109/var/www/
+    scp -P 22 -r ../css david@bytedojo.com:/var/lib/vz/private/109/var/www/
+    scp -P 22 -r ../js david@bytedojo.com:/var/lib/vz/private/109/var/www/
+    scp -P 22 -r ../images david@bytedojo.com:/var/lib/vz/private/109/var/www/
 fi
